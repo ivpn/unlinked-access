916359fefccfe50151ecb15750009a761ac35813
Juraj Hilje <juraj.hilje@gmail.com>
2025-07-09 09:46:07 +0200

    ci(token): update compose.yml

--- services/token/service/token.go
+++ services/token/service/token.go
@@ -30,6 +30,8 @@ func New(hsm HSMClient, cfg config.Config) *Server {
 }
 
 func (s *Server) Start() error {
+	log.Printf("Starting token service on %s:%s", s.Cfg.Host, s.Cfg.Port)
+
 	lis, err := net.Listen("tcp", s.Cfg.Host+":"+s.Cfg.Port)
 	if err != nil {
 		log.Println(err)
@@ -45,8 +47,6 @@ func (s *Server) Start() error {
 		log.Println(err)
 		return err
 	}
-
-	log.Println("token service started")
 
 	return nil
 }
