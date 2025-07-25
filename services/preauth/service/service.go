1be277da8baa860d9722bfcca3b722de88336ac1
Juraj Hilje <juraj.hilje@gmail.com>
2025-07-25 14:42:50 +0200

    feat(preauth): create service.go

---
+++ services/preauth/service/service.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"context"
+	"time"
+
+	"ivpn.net/auth/services/preauth/config"
+)
+
+type Cache interface {
+	Set(context.Context, string, any, time.Duration) error
+	Get(context.Context, string) (string, error)
+	Del(context.Context, string) error
+	Incr(context.Context, string, time.Duration) error
+}
+
+type Service struct {
+	Cfg   config.Config
+	Cache Cache
+}
+
+func New(cfg config.Config, cache Cache) *Service {
+	return &Service{
+		Cfg:   cfg,
+		Cache: cache,
+	}
+}
+
+func (s *Service) AddPreAuth(ctx context.Context, accountId string) error {
+	return nil
+}
+
+func (s *Service) GetPreAuth(ctx context.Context, accountId string) error {
+	return nil
+}
