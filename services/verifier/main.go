2cafaa54d70d3fe0233d25723421c7bdfc1c4423
Juraj Hilje <juraj.hilje@gmail.com>
2025-07-27 12:22:58 +0200

    feat(verifier): update main.go

--- services/verifier/main.go
+++ services/verifier/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"ivpn.net/auth/services/verifier/config"
+	"ivpn.net/auth/services/verifier/service"
+)
 
 func main() {
-	log.Println("verifier service started")
+	cfg, err := config.New()
+	if err != nil {
+		log.Println(err)
+	}
+
+	service := service.New(cfg)
+	err = service.Start()
+	if err != nil {
+		log.Println(err)
+	}
 }
