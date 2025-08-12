735dbe9a7d3fa2decb967c7bb0cbde6b53b786c9
Juraj Hilje <juraj.hilje@gmail.com>
2025-08-12 11:49:23 +0200

    feat(token): update hsm.go

--- services/token/client/hsm.go
+++ services/token/client/hsm.go
@@ -8,12 +8,12 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/kms"
+	kmstypes "github.com/aws/aws-sdk-go-v2/service/kms/types"
 	"ivpn.net/auth/services/token/model"
 )
 
 var (
-	ErrEmptyInput    = "input string cannot be empty"
-	ErrGenerateToken = "failed to generate token"
+	ErrEmptyInput = "input string cannot be empty"
 )
 
 type HSM struct {
@@ -33,14 +33,27 @@ func NewHSM() (*HSM, error) {
 }
 
 func (h *HSM) Token(input string) (*model.HSMToken, error) {
-	// TODO: Implement HSM signing
 	if input == "" {
 		return nil, fmt.Errorf("%s", ErrEmptyInput)
 	}
 
-	inputHash := sha512.Sum512([]byte(input))
+	keyID := "kms-key-id"
+	digest := sha512.Sum512([]byte(input))
+	ctx := context.Background()
+
+	signInput := &kms.SignInput{
+		KeyId:            &keyID,
+		Message:          digest[:],
+		MessageType:      kmstypes.MessageTypeDigest,
+		SigningAlgorithm: kmstypes.SigningAlgorithmSpecRsassaPssSha256,
+	}
+
+	signOut, err := h.Client.Sign(ctx, signInput)
+	if err != nil {
+		return nil, fmt.Errorf("failed to sign input: %w", err)
+	}
 
 	return &model.HSMToken{
-		Token: base64.StdEncoding.EncodeToString(inputHash[:]),
+		Token: base64.StdEncoding.EncodeToString(signOut.Signature),
 	}, nil
 }
