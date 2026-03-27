9f87311353b1a8ff54ac2706c5660acc46c45f71
Juraj Hilje <juraj.hilje@gmail.com>
2026-03-27 14:09:18 +0100

    chore(distributor): update routes.go

--- services/distributor/api/routes.go
+++ services/distributor/api/routes.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/healthcheck"
 	"github.com/gofiber/fiber/v2/middleware/helmet"
 	"ivpn.net/auth/services/distributor/config"
@@ -9,6 +10,14 @@ import (
 )
 
 func (h *Handler) SetupRoutes(cfg config.APIConfig) {
+	h.Server.Get("/debug-ip", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"ip":              c.IP(),
+			"x-forwarded-for": c.Get("X-Forwarded-For"),
+			"x-real-ip":       c.Get("X-Real-IP"),
+		})
+	})
+
 	h.Server.Use(helmet.New())
 	h.Server.Use(healthcheck.New())
 	h.Server.Use(auth.NewIPFilter(cfg.ApiAllowIPs))
